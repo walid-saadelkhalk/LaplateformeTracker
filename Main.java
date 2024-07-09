16ad3d9767867bb6235c19a75f7e0caae200587c
mathis-serra <mathis.serra@laplateforme.io>
2024-07-09 09:13:55 +0200

    [FUNCTIONAL] The database is now connected to the code

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
-
-
 import java.sql.Connection;
 import java.sql.SQLException;
+import src.Modele.Database;
 
 public class Main {
     public static void main(String[] args) {
-        // Obtenir la connexion à la base de données
-        Connection connection = Database.getConnection();
         
-        // Vérifier si la connexion est établie
+        Connection connection = Database.getConnection();
+
+        
         if (connection != null) {
             System.out.println("Connected to the database!");
         } else {
             System.out.println("Failed to connect to the database.");
         }
-        
-        // Fermer la connexion si nécessaire
+
+       
         try {
             if (connection != null && !connection.isClosed()) {
                 connection.close();
--- src/Modele/Database.java
+++ src/Modele/Database.java
@@ -1,4 +1,4 @@
-package Modele;
+package src.Modele;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
