8d832a5ce8f58b1687bfd2c8a235a8b37cfc56d1
mathis-serra <mathis.serra@laplateforme.io>
2024-07-09 08:11:10 +0200

    [TRYING] trying to connect the database.java with the Database

---
+++ Modele/Database.java
@@ -0,0 +1,29 @@
+package Modele;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+               
+                String url = "jdbc:mysql://localhost:3306/Harvard";
+                String user = "root";
+                String password = "dominique59";
+                
+                
+                
+                connection = DriverManager.getConnection(url, user, password);
+                System.out.println("Connection established successfully.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failed to establish connection.");
+            }
+        }
+        return connection;
+    }
+}
