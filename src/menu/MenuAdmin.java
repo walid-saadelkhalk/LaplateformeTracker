427c6e871ef3edf7240293cbb597c4e730b1202a
mathis-serra <mathis.serra@laplateforme.io>
2024-07-11 12:15:41 +0200

    take code from dev

--- src/menu/MenuAdmin.java
+++ src/menu/MenuAdmin.java
@@ -1,13 +1,17 @@
-package src.menu;
+package menu;
+import java.util.Scanner; 
+import src.controler.Authentication;
 
-import java.util.Scanner;
-import src.model.AdminRepository;
+/*
+ * This class is responsible for the menu of the admin.
+ * It will prompt the admin to choose between different options.
+ * It will then call the respective method from the Authentication class.
+ */
 
 public class MenuAdmin {
-
-    public void menuAdmin(Scanner scanner) {
+    public void menuAdmin(Scanner scanner){
         boolean continueAdminLoop = true;
-
+        
         while (continueAdminLoop) {
             System.out.println("\nWELCOME ADMIN ANDERSON!");
             System.out.println("What do you want to do?");
@@ -15,43 +19,46 @@ public class MenuAdmin {
             System.out.println("2. Create a student");
             System.out.println("3. Update a student");
             System.out.println("4. Delete a student");
-            System.out.println("5. Enter a grade (Feature not yet implemented)");
-            System.out.println("6. See classes (Feature not yet implemented)");
+            System.out.println("5. Enter a grade");
+            System.out.println("6. See classes");
             System.out.println("7. All students");
             System.out.println("8. Quit");
             System.out.println("Enter your choice:");
 
             int adminChoice = scanner.nextInt();
-            scanner.nextLine(); 
-
-        switch (adminChoice) {
-            case 1:
-                AdminRepository.searchStudentById();
-                break;
-            case 2:
-                AdminRepository.createAccount();
-                break;
-            case 3:
-                AdminRepository.updateStudent();
-                break;
-            case 4:
-                AdminRepository.deleteStudent();
-                break;
-            case 5:
-                System.out.println("Enter a grade (Feature not yet implemented)");
-                break;
-            case 6:
-                System.out.println("See classes (Feature not yet implemented)");
-                break;
-            case 7:
-                AdminRepository.getAllStudents();
-                break;
-            case 8:
+            scanner.nextLine();
+            boolean authenticationMan = true;
+            
+            switch (adminChoice){
+                case 1:
+                    System.out.println("Search a student");
+                    break;
+                case 2:
+                    Authentication authentication = new Authentication();
+                    authentication.createAccount();
+                    break;
+                case 3:
+                    System.out.println("Update a student");
+                    break;
+                case 4:
+                    System.out.println("Delete a student");
+                    break;
+                case 5:
+                    System.out.println("Enter a grade");
+                    break;
+                case 6:
+                    System.out.println("See classes");
+                    break;
+                case 7:
+                    System.out.println("All students");
+                    break;
+                case 8:
                 System.out.println("waddup");
-                continueAdminLoop = false;
-                break;
-            default:
-                System.out.println("Invalid choice.");
+                    continueAdminLoop = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
             }
         }
     }
