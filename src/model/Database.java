e388c406fa2d39b664853f94e83263bc32d3d2fb
Dhjali <barbara.vanni@laplateforme.io>
2024-07-17 15:27:25 +0200

    [MODIFY] can be merge

--- src/model/Database.java
+++ src/model/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/Harvard";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Vcassis13*";
+    private static final String PASSWORD = "__PASSWORD__";
 
     public static Connection getConnection() throws SQLException {
         try {
